00e156b4fbe620ba397b4b90b7692252c2b49416
gavinvc <tzn4fx@virginia.edu>
2025-11-10 15:18:11 -0500

    more structure to offers page

--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import rehooz_square from "../rehooz-square.png";
 
 export default function Offers() {
+  const user = JSON.parse(localStorage.getItem("user"));
+
   return (
     <main className="page-content">
-      <h2>My Offers</h2>
-      <p>Offers you've made/received (placeholder).</p>
+      <h2>Listings</h2>
+      <div className="Offers-container">
+        {/* Sent offers */}
+        <div className="Offers-column">
+          <h3 className="column-title">Sent offers</h3>
+          <div className="scroll-box" aria-label="Sent offers">
+            <p>Coming soon...</p>
+          </div>
+        </div>
+
+        {/* Followed Listings */}
+        <div className="Offers-column">
+          <h3 className="column-title">Received Offers</h3>
+          <div className="scroll-box" aria-label="Received Offers">
+            <p>Coming soon...</p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
